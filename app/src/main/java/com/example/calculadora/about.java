757cec435c644139576e29b605049369f5afda8f
root <root@localhost.localdomain>
2021-01-25 18:44:31 -0400

    Agregando WebView en about

--- app/src/main/java/com/example/calculadora/about.java
+++ app/src/main/java/com/example/calculadora/about.java
@@ -2,6 +2,7 @@ package com.example.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class about extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class about extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.loadUrl("https://ryuyii.github.io/ExampleWeb/");
     }
 }
